18ed900eb9d93f3d49c407dea209ef3005c776c0
drimstor <70888287+drimstor@users.noreply.github.com>
2021-07-16 22:11:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+   $('.header__burger').click(function(event) {
+     $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+   $('li').on('click', function(){
+      $('.header__burger,.header__menu').removeClass('active');
+      $('body').toggleClass('lock');
+    })
+});
+
+
+
+// const windowScrollTop = window.pageYOffset;
+// console.log(windowScrollTop);
+
+
+// const about = document.querySelector('#about');
+// const getItemTopCoord = about.getBoundingClientRect().top;
+
+// const getItemTopDocumentCoord = getItemTopCoord + window.pageYOffset;
+
+// console.log(getItemTopCoord);
+// console.log(getItemTopDocumentCoord);
+
