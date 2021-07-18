f0c689937d525d95c6512d18d61ebbe8d8464094
Vyacheslav <70888287+drimstor@users.noreply.github.com>
2021-07-18 20:21:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ $(document).ready(function() {
      $('.header__burger,.header__menu').toggleClass('active');
       $('body').toggleClass('lock');
    });
-   $('li').on('click', function(){
+
+   $('a.scrollto').on('click', function(){
       $('.header__burger,.header__menu').removeClass('active');
       $('body').removeClass('lock');
     })
+
+    $('a.scrollto').click(function() {
+      var elementClick = $(this).attr('href')
+      var destination = $(elementClick).offset().top;
+      jQuery('html:not(:animated),body:not(:animated)').animate({
+         scrollTop: destination
+      }, 800);
+      return false;
+    })
+
+
 });
 
 
